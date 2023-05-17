b0cedea1b0d08a374f7e4fb0920536a7dbb40032
Tung Ngo <tungntgch220075.fpt.edu.vn>
2023-05-17 16:36:48 +0700

    ok

--- routes/index.js
+++ routes/index.js
@@ -1,22 +1,26 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
-
 //1. render ra form input
-router.get('/input', (req, res) => {
+router.get('/', (req, res) => {
   res.render('input');
 });
 
 // 2. Lấy dữ liệu từ form input
 router.post('/output', (req, res) => {
+  var student = req.body;
+  // var name = req.body.ten;
+  // var age = req.body.tuoi;
+  // var email = req.body.email;
+  // var dob = req.body.ngaysinh;
+
   // A. In ra console log
   console.log(req.body);
   // B. In ra web với res.send
-  res.send(req.body);
+  // res.send(req.body);
+  // C. render ra trang output kèm dữ lieu từ form
+  res.render('output', { student: student });
+  // res.render('output', { name : name, age: age, email: email, dob: dob });
 });
 
 module.exports = router;
