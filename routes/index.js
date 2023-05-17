47ac42b86d344117ace0d2869654389b77f95036
Tung Ngo <tungntgch220075.fpt.edu.vn>
2023-05-17 15:55:57 +0700

    ok

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,17 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+//1. render ra form input
+router.get('/input', (req, res) => {
+  res.render('input');
+});
+
+// 2. Lấy dữ liệu từ form input
+router.post('/output', (req, res) => {
+  // A. In ra console log
+  console.log(req.body);
+  // B. In ra web với res.send
+  res.send(req.body);
+});
+
 module.exports = router;
